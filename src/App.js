ce312aee1a2ecf2b781bee7716452ccfffa290b5
PREETHIMADDI <indumaddi9@gmail.com>
2023-10-22 20:27:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { RecordData } from './pages/record';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-light">Dummy Data</h1>
+      <RecordData/>
+    </div>
+  );
+}
+
+export default App;
